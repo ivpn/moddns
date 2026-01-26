9e4ab6e0d752f91a2a4518fc644bd181fdf0acf9
Maciek <tomczukmaciej@gmail.com>
2026-01-26 14:57:27 +0100

    chore(proxy): Fix code review comment
    Signed-off-by: Maciek <tomczukmaciej@gmail.com>

--- proxy/config/config.go
+++ proxy/config/config.go
@@ -222,7 +222,7 @@ func New() (*Config, error) {
 
 	servicesCatalogPath := strings.TrimSpace(os.Getenv("SERVICES_CATALOG_PATH"))
 	if servicesCatalogPath == "" {
-		servicesCatalogPath = "/opt/services/catalog.ymls"
+		servicesCatalogPath = "/opt/services/catalog.yml"
 	}
 	servicesCatalogReloadEveryStr := strings.TrimSpace(os.Getenv("SERVICES_CATALOG_RELOAD"))
 	if servicesCatalogReloadEveryStr == "" {
