bd487867e855a3997e38e4cdb5ad35486e5fa403
Maciek <tomczukmaciej@gmail.com>
2026-04-08 13:54:35 +0200

    feat(app): support ZLA signup flow
    Signed-off-by: Maciek <tomczukmaciej@gmail.com>

--- app/src/api/client/configuration.ts
+++ app/src/api/client/configuration.ts
@@ -90,6 +90,10 @@ export class Configuration {
         this.basePath = param.basePath;
         this.serverIndex = param.serverIndex;
         this.baseOptions = {
+            headers: {
+                ...param.baseOptions?.headers,
+                'User-Agent': "OpenAPI-Generator/typescript-axios"
+            },
             ...param.baseOptions
         };
         this.formDataCtor = param.formDataCtor;
